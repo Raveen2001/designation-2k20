9b11310b96c208d9fe0f933435693df006954bb7
Raveen <raveenanbarasan@gmail.com>
2020-11-02 12:55:47 +0530

    final

---
+++ assets/js/app.js
@@ -0,0 +1,42 @@
+
+	$(document).ready(function() {
+	  $('body').addClass('js');
+	  var $menu = $('#menu'),
+	    $menulink = $('.menu-link');
+	  
+	$menulink.click(function() {
+	  $menulink.toggleClass('active');
+	  $menu.toggleClass('active');
+	  return false;
+	});});
+	$('.owl-carousel').owlCarousel({
+	    loop:true,
+	    margin:30,
+	    autoplay:true,
+		autoplayTimeout:5000,
+		autoplayHoverPause:true,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        550:{
+	            items:2
+	        },
+	        750:{
+	            items:3
+	        },
+	        1000:{
+	            items:4
+	        },
+	        1200:{
+	            items:5
+	        }
+	    }
+	})
+
+	
+
+
+	
+
+
